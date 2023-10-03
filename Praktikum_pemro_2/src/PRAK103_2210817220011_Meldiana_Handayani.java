10f2e9c86515258cbed1e4068e08fb2447e90ab7
meldiana18 <115007174+meldiana18@users.noreply.github.com>
2023-10-03 14:51:24 +0800

    PRAKTIKUM1PEMROGRAMAN-II

---
+++ Praktikum_pemro_2/src/PRAK103_2210817220011_Meldiana_Handayani.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class PRAK103_2210817220011_Meldiana_Handayani {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan jumlah baris (N): ");
+        int N = scanner.nextInt();
+        System.out.print("Masukkan bilangan awal: ");
+        int bilanganAwal = scanner.nextInt();
+        int bilangan = bilanganAwal;
+        String output = "";
+        while (N > 0) {
+            if (bilangan % 2 != 0) {
+                output += bilangan + " ";
+                N--;
+            }
+            bilangan++;
+        }
+        output = output.trim();
+        System.out.println(output);
+        scanner.close();
+    }
+}
+
+
